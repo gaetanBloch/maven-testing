9ab9734b451434b5e705cdd35b8976467f595de8
gbloch <gaetan.bloch@gmail.com>
2021-02-25 18:24:14 +0100

    Initialized Maven project

---
+++ src/test/java/com/gbloch/MainTest.java
@@ -0,0 +1,11 @@
+package com.gbloch;
+
+public class MainTest {
+    public void test1() {
+        System.out.println("--- TEST 1 ---");
+    }
+
+    public void test2() {
+        System.out.println("--- TEST 2 ---");
+    }
+}
